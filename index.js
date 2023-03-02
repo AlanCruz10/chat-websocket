9953762ae3a4884e3dd499ccd3caaa7ba2f0bb92
Alan <213376@ids.upchiapas.edu.mx>
2023-03-02 00:54:44 -0600

    chat

---
+++ index.js
@@ -0,0 +1,207 @@
+const loginScreen = document.querySelector('.login-screen');
+const chatScreen = document.querySelector('.chat-container');
+const conectedUsersArea = document.querySelector('.chat-container');
+chatScreen.style.display = 'none';
+conectedUsersArea.style.display = 'none';
+
+
+const socket = io();
+
+socket.on('user-connected', ( users ) => {
+    conectedUsers.innerHTML = '';
+    for (const user in users) {
+        conectedUsers.insertAdjacentHTML('beforeend',`<li>${user}</li>`);
+    }
+})
+
+socket.on('login', ( ) => {
+    alert('Bienvenido al Chat, respeta las reglas!')
+    loginScreen.style.display = 'none';
+    chatScreen.style.display = 'inline';
+    conectedUsersArea.style.display = 'inline';
+})
+
+socket.on('login-issue', () => {
+    alert('El nombre que intentas usar no esta disponible, intenta uno nuevo');
+    usernameInput.value = '';
+})
+
+usernameButton.addEventListener('click', ()=>{
+    let username = usernameInput.value;
+    socket.emit('register', username);
+});
+
+      /*const username = window.prompt("Usuario:");
+      socket.emit("new user", username);*/
+      
+     // let selectedUser = null;
+          
+      /*const userForm = document.getElementById("user_form");
+      const userTextAreaLogin = document.getElementById("text_area_login");
+      userForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        socket.emit("new user", userTextAreaLogin.value);
+      });*/
+
+      //const userList = document.getElementById("user_list");
+
+      socket.on("user list", (users) => {
+        const userList = document.getElementById("user_list");
+        userList.innerHTML = "";
+        for (let i = 0; i < users.length; i++) {
+          if (users[i] !== userTextAreaLogin.value) {
+            const userItem = document.createElement("li");
+            //console,log(userItem)
+            //userItem.classList.add("user_item");
+            userItem.textContent = users[i];
+            userItem.addEventListener("click", () => {
+                selectedUser = users[i];
+                const title = document.getElementById("text_area");
+                title.textContent = "Chat con " + selectedUser.value;
+                textArea.appendChild(title)
+              });
+            userList.appendChild(userItem);
+          }
+        }
+
+        const userItems = document.querySelectorAll(".user_item");
+          userItems.forEach((userItem) => {
+            userItem.addEventListener("click", () => {
+              userItems.forEach((item) => item.classList.remove("selected-user"));
+              userItem.classList.add("selected-user");
+            });
+          });
+      });
+
+      /*const username = window.prompt("Usuario:");
+      socket.emit("new user", username);*/
+
+      const messageForm = document.getElementById("message_form");
+      const textInput = document.getElementById("text_area");
+      const fileInput = document.getElementById("file_input");
+
+
+      fileInput.addEventListener('change', function(e) {
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+          const fileContent = event.target.result;
+          //fileDisplayArea.innerText = fileContent;
+        }
+
+        reader.readAsText(file);
+      });
+
+      messageForm.addEventListener("submit", function (e) {
+        const file = fileInput.files[0];
+
+        e.preventDefault();
+        if (textInput.value || fileInput.files.length > 0) {
+          const reader = new FileReader();
+
+        reader.onload = function(e) {
+          const fileContent = e.target.result;
+          let  data = {
+            "message":textInput.value,
+            "file":fileContent,
+            "user":selectedUser
+          }
+          socket.emit("new message", data);
+          //fileDisplayArea.innerText = fileContent;
+        }
+
+        reader.readAsText(file);
+         // const formData = new FormData();
+          /*let  data = {
+            "message":textInput.value,
+            "file":fileContent,
+            "user":selectedUser
+          }*/
+          /*if (textInput.value) {
+            formData.append("message", textInput.value);
+          }
+          if (fileInput.files.length > 0) {
+            formData.append("file", fileInput.files[0]);
+          }
+          if(textInput.value && fileInput.files.length > 0){
+            selectedUser && formData.append("to", selectedUser);
+          }
+          console.log(data)*/
+          //socket.emit("new message", data);
+          textInput.value = "";
+          selectedUser = null;
+          fileInput.value = "";
+          //fileDisplayArea.innerText = "";
+        }
+      });
+
+      socket.on("send message", (data) => {
+        const messageList = document.getElementById("message_list");
+        const chatItem = document.createElement("textarea");
+        const chatItemFile = document.createElement("div");
+        /*const chatItemFile = document.createElement("file");
+        const chatItemFile2 = document.createElement("text");*/
+        //chatItem.classList.add("message_list_textarea");
+        //console.log(data.message)
+        chatItem.textContent = data.user + ": " + data.message;
+        chatItemFile = data.file;
+        //chatItemFile.textContent = data.file;
+        /*if (data.file) {
+          const link = document.createElement("a");
+          link.href = data.file;
+          link.download = data.filename || "file";
+          link.innerText = "Descargar archivo";
+          chatItemFile.appendChild(link);
+        }*/
+        //chatItemFile2.textContent = data. file;
+        //chatItemFile2.file = data.file;
+        messageList.appendChild(chatItem);
+        messageList.appendChild(chatItemFile);
+        //messageList.appendChild(chatItemFile2);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+
+      socket.on("private message", (data) => {
+        const messageList = document.getElementById("message_list");
+        const chatItem = document.createElement("li");
+        chatItem.textContent = `De ${data.from}: ${data.message}`;
+        messageList.appendChild(chatItem);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+
+      socket.on("new message", (data) => {
+        const file = data.get("file");
+        if (file) {
+          console.log("Received file:", file.name);
+          // Do something with the file, e.g. save it to disk
+        }
+        const message = data.get("message");
+        if (message) {
+          if (data.has("to")) {
+            const toSocket =
+              io.sockets.connected[
+                connectedUsers.find((user) => user === data.get("to"))?.socketId
+              ];
+            if (toSocket) {
+              toSocket.emit("private message", {
+                from: socket.username,
+                message,
+              });
+              socket.emit("private message", {
+                from: socket.username,
+                message,
+                to: data.get("to"),
+              });
+            } else {
+              socket.emit("send message", {
+                message: "Usuario no encontrado",
+                user: "Sistema",
+              });
+            }
+          } else {
+            io.emit("send message", { message, user: socket.username });
+          }
+        }
+      });
+      
---
+++ server.js
@@ -0,0 +1,109 @@
+const application = require('express')();
+const server = require('http').createServer(application)
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000
+
+let connectedUsers = [];
+
+application.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(PORT, () => {
+   console.log('Servidor ejecutando en puerto: ' + PORT);
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('register', ( username ) => {
+        if ( connectedUsers[username] ) {
+           socket.emit('login-failed');
+           console.log('Usuario ya esta conectado - Usuario: ' + username);
+           return;
+        } else {
+           connectedUsers[username] = socket.id;
+           socket.username = username;
+           socket.emit('login');
+           io.emit('user-connected', connectedUsers);
+           console.log('Usuario conectado - Usuario: ' + socket.username)
+        }
+     });
+  
+
+   socket.on('disconnect', () => {
+       console.log('Usuario desconectado - Usuario: ' + socket.username);
+       connectedUsers = connectedUsers.filter(user => user !== socket.username);
+       io.emit('user list', connectedUsers);
+   });
+
+   socket.on('new message', (msg) => {
+       if (msg.user != null) {
+           const toSocket = io.sockets.connected[connectedUsers.find(user => user === msg.to)?.socketId];
+           if (toSocket) {
+               toSocket.emit('private message', { from: socket.username, message: msg.message });
+               socket.emit('private message', { from: socket.username, message: msg.message, to: msg.to });
+           } else {
+               socket.emit('send message', { message: 'Usuario no encontrado', user: 'Sistema' });
+           }
+       } else {
+            console.log(msg.user)
+            console.log(msg.message)
+            //console.log(msg.file)
+           io.emit('send message', { message: msg.message, user: socket.username, file: msg.file});
+       }
+   });
+
+   socket.on('new user', (usr) => {
+    const userExists = connectedUsers.find((user) => user.toLowerCase() === usr.toLowerCase());
+    if (userExists) {
+        const validation = true
+        io.emit('user validation', validation)
+        console.log('Usuario ya esta conectado - Usuario: ' + usr);
+    } else {
+        const validation = false
+        socket.username = usr;
+        connectedUsers.push(usr);
+        io.emit('user list', connectedUsers);
+        console.log('Usuario conectado - Usuario: ' + socket.username);
+        io.emit('user validation', validation)
+    }
+  });
+
+});
+    setInterval(() => {
+        io.emit('user list', connectedUsers);
+        }, 1000);
+    /*let ws;
+let chatForm = document.getElementById("chat-form");
+let chatMessages = document.getElementById("chat-messages");
+let chatInput = document.getElementById("chat-input");
+
+function connect() {
+    ws = new WebSocket("ws://" + document.location.host + "/chat");
+    ws.onmessage = function(event) {
+        let message = JSON.parse(event.data);
+        showMessage(message);
+    }
+}
+
+function sendMessage() {
+    let message = {
+        name: "User",
+        message: chatInput.value
+    }
+    ws.send(JSON.stringify(message));
+    chatInput.value = "";
+}
+
+function showMessage(message) {
+    let messageElement = document.createElement("div");
+    messageElement.innerHTML = "<strong>" + message.name + ": </strong>" + message.message;
+    chatMessages.appendChild(messageElement);
+}
+
+chatForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    sendMessage();
+});
+
+connect();*/
